83ccd201344432aadbfb191e31cd8beef5427d40
lonoil <129983959+lonoil@users.noreply.github.com>
2024-05-24 12:25:49 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Warnock Pro",
-        body: "Source Sans Pro",
+        body: "Warnock Pro",
         code: "IBM Plex Mono",
       },
       colors: {

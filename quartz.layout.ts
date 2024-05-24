63f6091c8111c8568a59a549a41a3096a537e76c
lonoil <129983959+lonoil@users.noreply.github.com>
2024-05-24 16:57:59 +0200

    Quartz sync: May 24, 2024, 4:57 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,7 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.Graph(),
-    Component.DesktopOnly(Darkmode()),
+    Component.DesktopOnly(Component.Darkmode()),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],

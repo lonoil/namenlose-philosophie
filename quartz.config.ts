7912ddcde2b69a23febedc4bb76d26311038cb78
lonoil <129983959+lonoil@users.noreply.github.com>
2024-05-24 12:07:50 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Warnock Pro",
+        header: "Playfair Display",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },

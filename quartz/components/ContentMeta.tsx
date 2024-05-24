8cbb6c4200df676b81a49f9c58d235f05f187a2d
lonoil <129983959+lonoil@users.noreply.github.com>
2024-05-24 17:02:57 +0200

    Quartz sync: May 24, 2024, 5:02 PM

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -34,7 +34,7 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
       }
 
       // Display reading time if enabled
-      if (options.showReadingTime) {
+      if (!options.showReadingTime) {
         const { minutes, words: _words } = readingTime(text)
         const displayedTime = i18n(cfg.locale).components.contentMeta.readingTime({
           minutes: Math.ceil(minutes),

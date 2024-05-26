08da064dffcb63b5347c734189f810bb6ae8fd2f
lonoil <129983959+lonoil@users.noreply.github.com>
2024-05-26 10:52:25 +0200

    Quartz sync: May 26, 2024, 10:52 AM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -7,7 +7,7 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   const baseDir = url.pathname
 
   return (
-    <article class="popover-hint">
+    <article class="popover-hint" style="padding-bottom: 20px;">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>

0178442428842510439fdff25ef6ad969eddc225
lonoil <129983959+lonoil@users.noreply.github.com>
2024-05-28 16:05:11 +0200

    Quartz sync: May 28, 2024, 4:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "warnock-pro",
-        body: "Calibri",
+        body: "calibri",
         code: "IBM Plex Mono",
       },
       colors: {
